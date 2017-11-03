8c3cb41d88d62849f9eba7b58dd1e11989a5404f
Y.Mahajan <mahajany@gmail.com>
2017-11-03 14:05:03 +0530

    Mongoose - sub documents; and fixed a typo in timestamp

--- 13NodeMongoose/Model/dishes.js
+++ 13NodeMongoose/Model/dishes.js
@@ -1,11 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const commentSchema = new Schema({
+    rating:     { type: Number, min:1, max:5, requied: true },
+    comment:    { type: String, required:true } ,
+    author:     { type: String, required: true }
+},
+                { timestamps: true }
+);
 const dishSchema = new Schema({
     name:           { type: String, required: true, unique: true},
-    description:    { type: String, required: true, unique: true}
+    description:    { type: String, required: true, unique: true},
+    comments:       [commentSchema]
 },
-                    { timetamps:true} //Automatically add cratedAt and updateAt
+                    { timestamps: true} //Automatically add cratedAt and updateAt
 
 );
 var Dishes = mongoose.model('Dish', dishSchema); //This will pick up collection "dishes" automagically
--- 13NodeMongoose/indexODM2.js
+++ 13NodeMongoose/indexODM2.js
@@ -13,27 +13,35 @@ dbConnection.then((db) => {
         name: 'Pizza',
         description: 'Yummy Yuymmy, fat Tymmy'
     })
-    .then((dish) => {
+        .then((dish) => {
             console.log("2.Added dish sucessfully ");
-            return Dishes.find({}).exec();
-    })
-    .then((dishes) => {
-            console.log("3. Complete dishes collection:" + dishes);
+            //return Dishes.find({}).exec();
+            return Dishes.findByIdAndUpdate(dish._id, {$set: {description: 'Updated...for testing'}
+        }, { new: true}).exec();
+        })
+        .then((dishes) => {
+            console.log("3. Complete dishes collection - after update:" + dishes);
+            dishes.comments.push({rating:5, comment: " I liked it", author:"YM"});
+            return dishes.save();
+        }). then((dishes) =>{
+            console.log("4. Pushed a comment in dishes collection:" + dishes);
             return db.collection('dishes').drop();
-    }).
-    then(() => {
-            console.log("4. Collection 'dishes' got dropped successfully");
+        }).
+        then(() => {
+            console.log("5. Collection 'dishes' got dropped successfully");
             return db.close();
-    })
-    .then(() => {
-            console.log("5. Last step of my journey...db.close");
-    })
-    .catch((err) => {
+        })
+        .then(() => {
+            console.log("6. Last step of my journey...db.close");
+        })
+        .catch((err) => {
             console.log("ERROR:" + err);
-    });
+            return;
+        });
     console.log("---this may get printed BEFORE 5-----");
 })
     .catch((err) => {
         console.log("DB CONNECTION ERROR: " + err);
+        return;
     });
 ;
